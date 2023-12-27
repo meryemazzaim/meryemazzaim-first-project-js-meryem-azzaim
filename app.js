2fc17883bd568a902f789b342bf1f05d0a034c0e
meryemazzaim <zaimmaryame2@gmail.com>
2023-12-27 09:11:50 +0100

    partie Withdraw Money and Deposit Money

--- app.js
+++ app.js
@@ -1,118 +1,118 @@
 
-// let login = prompt(" if you signing up ? or  ogging in ! or if you changing the password.")
+let login = prompt(" if you signing up ? or  ogging in ! or if you changing the password.")
 
-// if (login = "exit") {
-//     prompt(" if you signing up ? or  ogging in ! or if you changing the password.")
-// } if (login = "signup") {
-//     prompt(" Data of the following information ")
-
-
-// }
-
-// let Database = [
-//     {
-//         fullName: "azzaim",
-//         userName: "meryem",
-//         userEmail: "azzaimmeryem@gmail.com",
-//         userPassword: "password",
-//         userage: "age",
-//         Withdraw: 1000,
-//         Deposit: "",
-
-//     }
-// ];
-
-// //*parti 1 fullName userName  
-
-// let fullName = prompt("what is your fullName ?")
-// prompt(fullName.charAt(0).toUpperCase() + fullName.substring(1));
+if (login = "exit") {
+    prompt(" if you signing up ? or  ogging in ! or if you changing the password.")
+} if (login = "signup") {
+    prompt(" Data of the following information ")
 
 
-// let userName = prompt("what is your userName ?")
+}
 
-// while (userName.length < 5) {
-//     userName = prompt("the  userName should containe  5 characters ! ")
-// }
+let Database = [
+    {
+        fullName: "azzaim",
+        userName: "meryem",
+        userEmail: "azzaimmeryem@gmail.com",
+        userPassword: "password",
+        userage: "age",
+        Withdraw: 1000,
+        Deposit: "",
 
-// // userName = userName.trim()
-// prompt(userName.charAt(0).toUpperCase() + userName.slice(1));
+    }
+];
 
-// // let namefullname = ` ${userName} ${fullName}`;
-// // prompt(namefullname);
+//*parti 1 fullName userName  
 
-// //* parti 2  userEmail and password
-// let userEmail = prompt("insert your email ?")
-// for (let index = 0; index < userEmail.length; index++) {
-//     let element = userEmail[index];
-//     if (userEmail.length < 10) {
-//         userEmail = prompt(" insert your email the  userEmail should containe  10 characters !")
-
-//     }
-//     // else if (userEmail = ("@@")) {
-//     //     userEmail = prompt(" insert your email the  userEmail should containe  10 characters !")
-
-//     // }
-
-// } prompt(userEmail.toLocaleLowerCase());
+let fullName = prompt("what is your fullName ?")
+prompt(fullName.charAt(0).toUpperCase() + fullName.substring(1));
 
 
-// let userage = prompt("What is  your real age ?")
+let userName = prompt("what is your userName ?")
 
-// if (userage.length >= 3) {
-//     userage = prompt(" insert your real age should containe 2 number?")
+while (userName.length < 5) {
+    userName = prompt("the  userName should containe  5 characters ! ")
+}
 
+// userName = userName.trim()
+prompt(userName.charAt(0).toUpperCase() + userName.slice(1));
 
-// } else if (userage.length = ("string")) {
-//     prompt("  is not number?")
+// let namefullname = ` ${userName} ${fullName}`;
+// prompt(namefullname);
 
+//* parti 2  userEmail and password
+let userEmail = prompt("insert your email ?")
+for (let index = 0; index < userEmail.length; index++) {
+    let element = userEmail[index];
+    if (userEmail.length < 10) {
+        userEmail = prompt(" insert your email the  userEmail should containe  10 characters !")
 
-// }
-// // (userage.length = ("string"))
+    }
+    // else if (userEmail = ("@@")) {
+    //     userEmail = prompt(" insert your email the  userEmail should containe  10 characters !")
 
+    // }
 
-
-// let userPassword = prompt("insert your password  the  password should containe 7 characters")
-// while (userPassword.length <= 7) {
-//     userPassword = prompt("the  password should containe 7 characters !")
-
-// }
-// //*partie 3  Password_confirmed:
-// let connected = [];
-
-// const User = (password) => {
-
-//     let connectUser = Database.filter(element => (element.userPassword === password))
-//     console.log(connectUser);
-
-
-//     if (connected.length == 0 && connectUser) {
-//         connected.push(connectUser)
-//         console.log("you are connected");
-//     } else {
-//         alert("the  info  not match our")
-//     }
-
-// }
+} prompt(userEmail.toLocaleLowerCase());
 
 
-// let loginPassword = prompt("Insert your password to login")
-// while (loginPassword.length == 0) {
-//     loginPassword = prompt("password Insert your password to login")
-// }
+let userage = prompt("What is  your real age ?")
 
-// User(loginPassword)
+if (userage.length >= 3) {
+    userage = prompt(" insert your real age should containe 2 number?")
 
 
-// let login2 = prompt(" if you signing up ? or  ogging in ! or if you changing the password.")
-
-// if (login2 = "exit") {
-//     prompt(" if you signing up ? or  ogging in ! or if you changing the password.")
-// } else {
-//     prompt(" Data of the following information ")
-// }
+} else if (userage.length = ("string")) {
+    prompt("  is not number?")
 
 
- //*partie Withdraw Money and Deposit Money
+}
+// (userage.length = ("string"))
+
+
+
+let userPassword = prompt("insert your password  the  password should containe 7 characters")
+while (userPassword.length <= 7) {
+    userPassword = prompt("the  password should containe 7 characters !")
+
+}
+//*partie 3  Password_confirmed:
+let connected = [];
+
+const User = (password) => {
+
+    let connectUser = Database.filter(element => (element.userPassword === password))
+    console.log(connectUser);
+
+
+    if (connected.length == 0 && connectUser) {
+        connected.push(connectUser)
+        console.log("you are connected");
+    } else {
+        alert("the  info  not match our")
+    }
+
+}
+
+
+let loginPassword = prompt("Insert your password to login")
+while (loginPassword.length == 0) {
+    loginPassword = prompt("password Insert your password to login")
+}
+
+User(loginPassword)
+
+
+let login2 = prompt(" if you signing up ? or  ogging in ! or if you changing the password.")
+
+if (login2 = "exit") {
+    prompt(" if you signing up ? or  ogging in ! or if you changing the password.")
+} else {
+    prompt(" Data of the following information ")
+}
+
+
+//*partie Withdraw Money and Deposit Money
 let ask_option = prompt("give me a chooses this option !")
 
 function option(params) {
@@ -127,19 +127,21 @@ function option(params) {
 
 option(ask_option)
 
+// let ask_option2 = prompt(" Take a Loan!")
 
-function isEven(params) {
 
-    if (params=1000 % 20) {
-        alert("thie number you picked is Even")
-    } else if (params==1000 % 10) {
+// function Loan(params) {
 
-        alert("thie number you picked is Odd")
-    } else {
-        alert("thats not  a number")
-    }
-}
+//     if (params % 20 ==1200) {
+//         alert("thie number 1200 ")
+//     } else if (params  % 10==1000) {
 
-isEven(askNumber)
+//         alert("thie number 1000")
+//     } else {
+//         alert("thats  number")
+//     }
+// }
+
+// Loan(ask_option2);
 
 
